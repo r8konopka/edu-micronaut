47301a27d82bd8ddcdbd94d662094248ef18d047
rkonopka <radoslaw.konopka@softwarehut.com>
2020-04-21 13:41:12 +0200

    Added Basic Authentication

---
+++ src/main/java/com/softwarehut/edu/AuthenticationProviderUserPassword.java
@@ -0,0 +1,27 @@
+package com.softwarehut.edu;
+
+import org.reactivestreams.Publisher;
+
+import java.util.ArrayList;
+
+import javax.inject.Singleton;
+
+import io.micronaut.security.authentication.AuthenticationFailed;
+import io.micronaut.security.authentication.AuthenticationProvider;
+import io.micronaut.security.authentication.AuthenticationRequest;
+import io.micronaut.security.authentication.AuthenticationResponse;
+import io.micronaut.security.authentication.UserDetails;
+import io.reactivex.Flowable;
+
+@Singleton
+public class AuthenticationProviderUserPassword implements AuthenticationProvider {
+
+    @Override
+    public Publisher<AuthenticationResponse> authenticate(AuthenticationRequest authenticationRequest) {
+        if (authenticationRequest.getIdentity().equals("micronaut") &&
+                authenticationRequest.getSecret().equals("edu") ) {
+            return Flowable.just(new UserDetails((String) authenticationRequest.getIdentity(), new ArrayList<>()));
+        }
+        return Flowable.just(new AuthenticationFailed());
+    }
+}
--- src/main/java/com/softwarehut/edu/CompanyController.java
+++ src/main/java/com/softwarehut/edu/CompanyController.java
@@ -8,7 +8,9 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.security.annotation.Secured;
 
+@Secured("isAuthenticated()")
 @Controller("/company")
 public class CompanyController {
 
